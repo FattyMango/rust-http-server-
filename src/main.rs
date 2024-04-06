b7b661c9d28607c20f5ac304da127bcc40c88515
Mahmoud Kassab <mahmoud.k@hybridsolutions.com>
2024-04-07 01:52:47 +0300

    Refactor handle_connection function to handle different HTTP requests in main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,41 +2,46 @@ use std::{
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
+use hello::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
+
+
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let response: String;
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = std::fs::read_to_string("src/templates/index.html").unwrap();
+        let length = contents.len();
 
+        response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+        );
 
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|line| {
-            println!("hi");
-            return line.unwrap();
-        })
-        .take_while(|line| {
-            println!("hello");
-            return !line.is_empty();
-        })
-        .collect();
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let content = "404 NOT FOUND";
+        let length =content.len();
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = std::fs::read_to_string("src/templates/index.html").unwrap_or_else(|err| {
-        println!("Error reading file: {}", err);
-        std::process::exit(500);});
-    let length = contents.len();        
-    let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{content}"
+        );
 
-    stream.write_all(response.as_bytes()).unwrap();
-    println!("Request: {:#?}", http_request);
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
